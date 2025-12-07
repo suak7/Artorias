358975aa864ec7f967c97e1f962b3cc4757ae9ad
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-06 19:18:24 -0500

    set up gdt

---
+++ boot/gdt.asm
@@ -0,0 +1,32 @@
+[bits 16]
+
+gdt_start:
+
+gdt_null:
+    dd 0x00000000           
+    dd 0x00000000           
+
+gdt_code:
+    dw 0xffff               
+    dw 0x0000               
+    db 0x00                 
+    db 10011010b            
+    db 11001111b            
+    db 0x00                 
+
+gdt_data:
+    dw 0xfff               
+    dw 0x0000               
+    db 0x00                 
+    db 10010010b            
+    db 11001111b         
+    db 0x00                
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1  
+    dd gdt_start                
+
+CODE_SEG equ gdt_code - gdt_start  
+DATA_SEG equ gdt_data - gdt_start  
