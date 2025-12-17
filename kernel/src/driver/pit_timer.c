4058a08945166b109930fed09681128a2d423c00
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-17 00:44:43 -0500

    fix braces

--- kernel/src/driver/pit_timer.c
+++ kernel/src/driver/pit_timer.c
@@ -1,10 +1,12 @@
 #include "driver/pit_timer.h"
 #include <ports.h> 
 
-void pit_wait(uint32_t ms) {
+void pit_wait(uint32_t ms) 
+{
     uint32_t total_ticks = ms * (PIT_BASE_FREQ / 1000);
 
-    while (total_ticks > 0) {
+    while (total_ticks > 0) 
+    {
         uint16_t current_chunk = (total_ticks > 0xFFFF) ? 0xFFFF : (uint16_t)total_ticks;
 
         outb(PIT_COMMAND, 0x30); 
@@ -12,7 +14,8 @@ void pit_wait(uint32_t ms) {
         outb(PIT_CHANNEL_0, (uint8_t)((current_chunk >> 8) & 0xFF));
 
         uint8_t status = 0;
-        do {
+        do 
+        {
             outb(PIT_COMMAND, 0xE2); 
             status = inb(PIT_CHANNEL_0);
         } while (!(status & 0x80)); 
